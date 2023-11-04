f0d5f2472fb06ce7ba6ac4b971f49359bd9546f3
Simon Mitternacht <simon.mitternacht@gmail.com>
2023-11-04 13:52:47 +0100

    Update to Sanity 3.

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,9 +1,3 @@
-// First, we must import the schema creator
-import createSchema from "part:@sanity/base/schema-creator";
-
-// Then import schema types from any plugins that might expose them
-import schemaTypes from "all:part:@sanity/base/schema-type";
-
 import recordLabel from "./recordLabel";
 import person from "./person";
 import contactInfo from "./contactInfo";
@@ -15,21 +9,15 @@ import video from "./video";
 import frontMatter from "./frontMatter";
 import link from "./link";
 
-// Then we give our schema to the builder and provide the result to Sanity
-export default createSchema({
-  name: "default",
-  // Then proceed to concatenate our our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    album,
-    song,
-    appearsOn,
-    concert,
-    video,
-    frontMatter,
-    contactInfo,
-    recordLabel,
-    person,
-    link,
-  ]),
-});
+export const schemaTypes = [
+  album,
+  song,
+  appearsOn,
+  concert,
+  video,
+  frontMatter,
+  contactInfo,
+  recordLabel,
+  person,
+  link,
+];
