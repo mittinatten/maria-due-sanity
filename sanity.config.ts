6efc90be0b709dfee41a50bcf38626fa7d793dd3
Simon Mitternacht <simon.mitternacht@gmail.com>
2025-09-06 11:57:27 +0200

    Clean up after upgrade.

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,22 +1,15 @@
-import { defineConfig, defaultTheme } from 'sanity';
+import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure'
 
 import { schemaTypes } from './schemas/schema.js'
 
 export default defineConfig({
-  "project": {
-    "name": "Maria Due"
+  project: {
+    name: "Maria Due"
   },
-  "projectId": "zp0hhhsi",
-  "dataset": "production",
-  "plugins": [
-    structureTool(),
-    // "@sanity/base",
-    // "@sanity/components",
-    // "@sanity/default-layout",
-    // "@sanity/default-login",
-    // "@sanity/desk-tool"
-  ],
+  projectId: "zp0hhhsi",
+  dataset: "production",
+  plugins: [structureTool()],
   schema: {
     types: schemaTypes
   },
